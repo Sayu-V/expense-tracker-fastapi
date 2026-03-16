adcac7bb94c7cc77ad0b1e2217d06f4e5480819a
SYUHHHP\SAYOOJ <sayujrsayu@gmail.com>
2026-03-16 17:51:01 +0530

    no message

--- app/schemas/expense_schema.py
+++ app/schemas/expense_schema.py
@@ -3,14 +3,18 @@ from enum import Enum
 from app.storage.memory_db import categories
 
 
-# Build enum dictionary
-category_dict = {c["name"]: c["name"] for c in categories}
-
-# Create Enum dynamically
-CategoryEnum = Enum("CategoryEnum", category_dict, type=str)
+# build enum
+CategoryEnum = Enum(
+    "CategoryEnum",
+    {c["name"]: c["name"] for c in categories},
+    type=str
+)
 
 
 class Expense(BaseModel):
     category: CategoryEnum
     amount: float
     note: str | None = None
+
+    class Config:
+        use_enum_values = True
