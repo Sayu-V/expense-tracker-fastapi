166568cec9a90883217932b2284be9616c0ee4a9
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-16 16:47:28 +0530

    Update Expense schema to use CategoryEnum
    Refactor Expense schema to use dynamic Enum for categories.

--- app/schemas/expense_schema.py
+++ app/schemas/expense_schema.py
@@ -1,35 +1,14 @@
-"""
-from pydantic import BaseModel
-from datetime import date
-
-class Expense(BaseModel):
-    category: str
-    amount: float
-    note: str = ""
-    date: str = str(date.today())
--------
-from pydantic import BaseModel
-from app.storage.memory_db import categories
-
-# 👇 THIS LINE CREATES DROPDOWN VALUES
-category_names = [c["name"] for c in categories]
-
-class Expense(BaseModel):
-    category: str
-    amount: float
-    note: str | None = None
-"""
-
 from pydantic import BaseModel
 from enum import Enum
 from app.storage.memory_db import categories
 
+# Build enum values from category list
+class CategoryEnum(str, Enum):
+    pass
 
-# 🔹 Dynamically create Enum from category list
-CategoryEnum = Enum(
-    "CategoryEnum",
-    {c["name"]: c["name"] for c in categories}
-)
+# Dynamically add categories to Enum
+for c in categories:
+    setattr(CategoryEnum, c["name"], c["name"])
 
 class Expense(BaseModel):
     category: CategoryEnum
