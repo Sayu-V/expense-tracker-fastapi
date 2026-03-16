790374b2f7d0552dc06ab7afae861ac3e06de622
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-17 05:07:20 +0530

    Update analytics.py

--- app/routers/analytics.py
+++ app/routers/analytics.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter, Depends
 from app.core.dependencies import get_analytics_service
+from app.schemas.api_response import APIResponse
 
 router = APIRouter(prefix="/analytics", tags=["Analytics"])
 
 
-@router.get("/top-categories")
+@router.get("/top-categories", response_model=APIResponse)
 def top_categories(service = Depends(get_analytics_service)):
-    return service.get_top_categories()
+    data = service.get_top_categories()
+
+    return APIResponse(
+        status="success",
+        data=data
+    )
 
 
 @router.get("/expense-trends")
