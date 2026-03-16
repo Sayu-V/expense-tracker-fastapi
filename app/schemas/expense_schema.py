fbb307fd37d561964f659ed95a9dff107fed2c02
SYUHHHP\SAYOOJ <sayujrsayu@gmail.com>
2026-03-16 17:20:54 +0530

    no message

--- app/schemas/expense_schema.py
+++ app/schemas/expense_schema.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 from enum import Enum
 from app.storage.memory_db import categories
 
-# Build enum values from category list
-class CategoryEnum(str, Enum):
-    pass
 
-# Dynamically add categories to Enum
-for c in categories:
-    setattr(CategoryEnum, c["name"], c["name"])
+# Build enum dictionary
+category_dict = {c["name"]: c["name"] for c in categories}
+
+# Create Enum dynamically
+CategoryEnum = Enum("CategoryEnum", category_dict, type=str)
+
 
 class Expense(BaseModel):
     category: CategoryEnum
