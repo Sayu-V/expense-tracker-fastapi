5fce2c1503cae5f797a8ae87c2cc0e451776b5ce
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-17 15:34:22 +0530

    Update expense_service.py

--- app/services/expense_service.py
+++ app/services/expense_service.py
@@ -25,3 +25,51 @@ def get_expenses(page, limit, category=None, min_amount=None, max_amount=None):
         "items": data[start:end],
         "total": total
     }
+
+
+def get_expense_by_id(expense_id: int):
+    for e in expenses:
+        if e["id"] == expense_id:
+            return e
+    return None
+
+
+def create_expense(expense, next_id, categories):
+    data = expense.dict()
+    data["category"] = data["category"].value
+
+    category_obj = next((c for c in categories if c["name"] == data["category"]), None)
+
+    if not category_obj:
+        return None, "Invalid category"
+
+    data["id"] = next_id
+    expenses.append(data)
+
+    return data, None
+
+
+def update_expense(expense_id: int, expense, categories):
+    data = expense.dict()
+
+    category_obj = next((c for c in categories if c["name"] == data["category"]), None)
+
+    if not category_obj:
+        return None, "Invalid category"
+
+    for e in expenses:
+        if e["id"] == expense_id:
+            e["category"] = data["category"]
+            e["amount"] = data["amount"]
+            e["note"] = data.get("note", "")
+            return e, None
+
+    return None, "Expense not found"
+
+
+def delete_expense(expense_id: int):
+    for e in expenses:
+        if e["id"] == expense_id:
+            expenses.remove(e)
+            return True
+    return False
