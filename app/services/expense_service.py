262b439ceb46844496a4f6b972b666f8cf0321f7
Sayooj Vandichal <sayujrsayu@gmail.com>
2026-03-17 18:08:39 +0530

    Update expense_service.py

--- app/services/expense_service.py
+++ app/services/expense_service.py
@@ -1,14 +1,15 @@
 from app.storage.memory_db import expenses, budget
 
-
 def get_expenses(page, limit, category=None, min_amount=None, max_amount=None):
+    from app.storage.memory_db import expenses
+
     data = expenses
 
-    # filter by category
+     # filter by category
     if category:
-        data = [e for e in data if e["category"].lower() == category.lower()]
+        data = [e for e in data if e["category"] == category]
 
-    # filter by amount
+     # filter by amount
     if min_amount is not None:
         data = [e for e in data if e["amount"] >= min_amount]
 
